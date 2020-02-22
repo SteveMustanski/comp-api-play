73b2c30f85ef06fe5401788ef451bf73d4b541bd
SteveMustanski <steve.mustanski@gmail.com>
2020-02-22 17:27:34 -0500

    added grocery list commonent and code to make it work

---
+++ src/components/GroceryList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="text" v-model="input" placeholder="Add Grocery" />
+    <input type="submit" @click="addGrocery()" />
+    <ul>
+      <li v-for="(item, index) in groceries" :key="item">
+        {{ item }}
+        <button @click="deleteGrocery(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        input: "",
+        groceries: []
+      }
+    },
+    methods: {
+      addGrocery() {
+        this.groceries.push(this.input);
+        this.input = "";
+      },
+      deleteGrocery(index) {
+        this.groceries.splice(index, 1);
+      }
+    },
+  }
+</script>
+
+<style lang="css" scoped>
+  ul {
+    list-style-type: none;
+  }
+</style>
